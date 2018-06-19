00c130f1869206b16c0d0fa34e30497f1ddc5271
gauravano <sachdeva.gaurav1997@gmail.com>
2018-06-20 01:27:41 +0530

    try

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'nokogiri'
 gem 'rails', '5.0.6'
 gem 'activerecord-session_store'
 gem 'passenger'
--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,7 +1,7 @@
 class WelcomeMailer < ActionMailer::Base
   helper :application
   include ApplicationHelper
-  # default from: "do-not-reply@#{ActionMailer::Base.default_url_options[:host]}"
+  default from: "gauravano97@gmail.com"
 
   # PasswordResetMailer.reset_notify(user).deliver_now
   def add_to_list(user, list)
@@ -9,5 +9,12 @@ class WelcomeMailer < ActionMailer::Base
     @list = list
     @footer = feature('email-footer')
     mail(to: list + '+subscribe@googlegroups.com', subject: subject, from: user.email)
+  end
+
+  def welcome_mail(user)
+    subject = 'Welcome to Public Lab'
+    @user = user
+    @footer = feature('email-footer')
+    mail(to: user.email, subject: subject, from: "do-not-reply@#{ActionMailer::Base.default_url_options[:host]}")
   end
 end
